22d9ff009140b298041a75a6b5a4b57c49317c22
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-02-25 23:01:58 +0200

    Part 02 EF Core Vs Dapper Vs ADO.NET

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,11 +4,19 @@
     {
         static void Main(string[] args)
         {
+
             #region Part 01 What is EF Core
 
             //Done In NoteBook.
 
             #endregion
+
+            #region Part 02 EF Core Vs Dapper Vs ADO.NET
+
+            //Done In NoteBook.
+
+            #endregion
+
         }
     }
 }
