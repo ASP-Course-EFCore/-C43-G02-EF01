c98591b90712b2ea5dd2e09d969f44817818a00a
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-02-25 23:01:15 +0200

    Part 01 What is EF Core

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 What is EF Core
+
+            //Done In NoteBook.
+
+            #endregion
         }
     }
 }
