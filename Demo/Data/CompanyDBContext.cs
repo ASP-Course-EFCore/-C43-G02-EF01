b25524e1991fa95048f2a3853b42ee953a3b5f7d
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-02-26 00:36:28 +0200

    Part 03 What is DbContext Class

---
+++ Demo/Data/CompanyDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Data
+{
+    class CompanyDBContext:DbContext
+    {
+        //If there is Inheritance RelationShip
+        //Any Constructor that i will make inside child class it chain by default on the constructor of the baseClass.
+        public CompanyDBContext():base() 
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Data Source = .; Initial Catalog = Company; UserId = sa; Password = 123456");//To Connect on sql server service using Sql Server Authentication using userName & password
+            //optionsBuilder.UseSqlServer("Data Source = .; Initial Catalog = Company; Integrated Security = true");//To Connect on sql server using Windows Authentication using My Laptop/Server Name.
+                                                                                                                    //This connection string syntax is legacy, so now they make new meaning full connection string
+                                                                                                                    // but this still work
+
+            optionsBuilder.UseSqlServer("Server = .; Database = Company; Trusted_Connection = true");//Trust App To connect on sql server service throw Windows authentication.
+        }
+        //So now the App is connected on sql server service throw my server/Laptop.
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Demo
+﻿using Demo.Data;
+
+namespace Demo
 {
     internal class Program
     {
@@ -14,6 +16,76 @@
             #region Part 02 EF Core Vs Dapper Vs ADO.NET
 
             //Done In NoteBook.
+
+            #endregion
+
+            #region Part 03 What is DbContext Class
+            ///CodeFirst Approach
+            ///1st Feature in EF Core => Automatic Schema Migration [Mapping]. 
+            ///We always first make folder called Data Contain => 
+            /// 1- DBContext Class => Name of it must end with "DBContext"
+            /// 2- Boko Classes || Domain Models|| Entities || Classes Represent structure of Tables,Views of Database
+            ///     Domain Models => Means The Models Specific to business domain
+            ///     Represent Models/Classes That will be Tables in the database of the busineess
+            ///     Like SuperMarket Business Has DomainModels Like => [Customers - Products - Sales] Tables
+            ///     So, objects that we retrieve it from DB called business objects
+            ///
+            ///     BokoClasses => each BokoClass represent table in DB - and Only Contain Properties, each property represent column in database 
+            ///
+            /// 3- Migrations 
+            ///
+            ///We need this "DBContext" class to have set of properties & methods
+            ///but we don't write it, we inherit it from base class "DBContext"
+            ///it will be the base class of any "DBContext" we will create.
+            ///This base Class "DBContext" is built in class in "EF Core", so we need first to install "EF Core" package and use namespace of it => "using Microsoft.EntityFrameworkCore;"
+            ///There are 2 ways to install it :
+            /// 1- By UI of VS => Throw "npm" of .NET which is "NuGet.org"
+            /// 2- Throw Package Manager Console => Install-Package Microsoft.EntityFrameworkCore.SqlServer [This will install the latest version of the package]
+            ///                                     Install-Package Microsoft.EntityFrameworkCore.SqlServer -v "8.0.15" [To install specific version].
+            ///Note => Version of "EF Core" package must be same like version of ".NET Core" 
+            ///After Install, You will found it in "Dependencies/Packages/Microsoft.EntityFrameworkCore.SqlServer (9.0.2)"
+            ///
+            ///The Important method that class "CompanyDBContext" inherit it from class "DBContext" is => "OnConfiguring" method.
+            ///Throw it we can specify the ConnectionString 
+            ///
+            ///When Connect on DB throw App:
+            ///- We make object from class "CompanyDBContext" that has empty parameterless constructor by default.
+            ///- We use try-finally because connection with DB is unmanaged by CLR, so we need to manage this resource using try-finally
+            ///  to release this resourse after finishing.
+            ///
+            ///CompanyDBContext context01 = new CompanyDBContext();
+            ///try
+            ///{
+            ///    //Code
+            ///}
+            ///finally
+            ///{
+            ///    //Release The Resourse [Connection].
+            ///    context01.Dispose();
+            ///}
+            ///
+            ///Or Use using block => using(){ } => it's syntax suger for try-finally.
+            ///
+            ///using (CompanyDBContext context02 = new CompanyDBContext())//It automatically release this object after finish.
+            ///{
+            ///    //Code.
+            ///}
+            ///
+            ///Or This another syntax sugar for using => using without curly brackets
+            ///
+            ///using CompanyDBContext context03 = new CompanyDBContext();
+            ///Code.[الكود دا مش مقيد ب اسكوب معين]
+            ///
+
+            //using CompanyDBContext context = new CompanyDBContext();
+
+            ///We can Create object from class "CompanyDBContext" using Dependency Injection Approach when i need this object in many place in code in same request because if i make throw parameterless constructor, every need to it i make different and new object.
+            ///this when i need this object like in 10 different places in program,
+            ///i will not every time i need this object, make new object from "CompanyDBContext" using parameterless constructor.
+            ///i will let CLR to create the object in run time when i need it, and every time i need object from class "CompanyDBContext"
+            ///i ask CLR to return it to me, but return the first object CLR create. 
+            ///So I have 10 place in program/App depend on object from class "CompanyDBContext" => Dependency.
+            ///And Run Time provide those objects to me throw injection => Injection.
 
             #endregion
 
